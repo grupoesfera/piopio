169f77d92a47b9980d1ed35acc959fa1f372decb
Marcelo Gore <marcelogore@gmail.com>
2018-02-21 22:34:00 -0300

    Expone un servicio para obtener pios

--- src/main/java/ar/com/grupoesfera/redlink/piopio/modelo/Pio.java
+++ src/main/java/ar/com/grupoesfera/redlink/piopio/modelo/Pio.java
@@ -1,5 +1,6 @@
 package ar.com.grupoesfera.redlink.piopio.modelo;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
@@ -8,4 +9,27 @@ public class Pio {
 
     @Id
     private Long id;
+    
+    @Column
+    private String mensaje;
+    
+    public Long getId() {
+    
+        return id;
+    }
+    
+    public void setId(Long id) {
+    
+        this.id = id;
+    }
+    
+    public String getMensaje() {
+    
+        return mensaje;
+    }
+    
+    public void setMensaje(String mensaje) {
+    
+        this.mensaje = mensaje;
+    }
 }
--- src/main/java/ar/com/grupoesfera/redlink/piopio/rest/API.java
+++ src/main/java/ar/com/grupoesfera/redlink/piopio/rest/API.java
@@ -2,9 +2,12 @@ package ar.com.grupoesfera.redlink.piopio.rest;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+
+import ar.com.grupoesfera.redlink.piopio.modelo.Pio;
 
 @Path("/")
 public class API {
@@ -15,5 +18,17 @@ public class API {
     public Response hola() {
         
         return Response.ok("Hola, mundo!").build();
+    }
+
+    @GET
+    @Path("/pio/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response obtenerPio(@PathParam("id") Long id) {
+        
+        Pio pio = new Pio();
+        pio.setId(id);
+        pio.setMensaje("Este es un pio autogenerado que no existe en la DB");
+        
+        return Response.ok(pio).build();
     }
 }
