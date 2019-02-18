54693c472c5f82b12f02e845cbdea84968e8ffef
Marcelo Gore <marcelogore@gmail.com>
2019-02-18 14:51:05 -0300

    Agrega una prueba para el servicio pio

--- src/test/java/ar/com/grupoesfera/piopio/UserAcceptanceTest.java
+++ src/test/java/ar/com/grupoesfera/piopio/UserAcceptanceTest.java
@@ -5,6 +5,7 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
+import org.apache.commons.logging.LogFactory;
 import org.apache.http.HttpStatus;
 import org.hamcrest.Matchers;
 import org.junit.Assert;
@@ -22,23 +23,38 @@ public class UserAcceptanceTest {
     }
     
     @Test
-    public void hola() throws Exception {
-
+    public void deberiaDecirHolaMundo() throws Exception {
+    	
         RespuestaServicio respuesta = invocarServicio("hola");
         Assert.assertThat(respuesta.getCodigo(), Matchers.is(HttpStatus.SC_OK));
+        Assert.assertThat(respuesta.getTexto(), Matchers.is("Hola, mundo!"));
     }
      
+    @Test
+    public void deberiaDarNotAllowedAlLlamarAPioSinParametros() throws Exception {
+
+        RespuestaServicio respuesta = invocarServicio("pio");
+        Assert.assertThat(respuesta.getCodigo(), Matchers.is(HttpStatus.SC_METHOD_NOT_ALLOWED));
+    }
+
     private RespuestaServicio invocarServicio(String urlServicio) throws Exception {
     	
     	final String urlBase = "http://localhost:8080/api/";
     	
     	RespuestaServicio respuesta = new RespuestaServicio();
-    	
-        URL url = new URL(urlBase + urlServicio);
-        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-        respuesta.setCodigo(connection.getResponseCode());
-        BufferedReader input = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-        respuesta.setTexto(input.readLine());
+    
+    	try {
+    		
+    		URL url = new URL(urlBase + urlServicio);
+    		HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+    		respuesta.setCodigo(connection.getResponseCode());
+    		BufferedReader input = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+    		respuesta.setTexto(input.readLine());
+
+    	} catch (Exception e) {
+    		
+    		LogFactory.getLog(UserAcceptanceTest.class).error(e);
+    	}
         
         return respuesta;
     }
