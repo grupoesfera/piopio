bfdfd0fb158fd01f1054ab08911d2fce64b1877a
Marcelo Gore <marcelogore@gmail.com>
2019-02-20 15:26:09 -0300

    Separa las pruebas de aceptación y agrega un manejador de estados del server para que funcione con las pruebas separadas

--- src/main/java/ar/com/grupoesfera/main/PioServer.java
+++ src/main/java/ar/com/grupoesfera/main/PioServer.java
@@ -12,30 +12,77 @@ import io.undertow.servlet.api.DeploymentInfo;
 public class PioServer {
 
     private static final Log log = LogFactory.getLog(PioServer.class);
+    private static final PioServer instancia = new PioServer();
+
+    private final UndertowJaxrsServer server;
+    private Estado estado;
+
+    private PioServer() {
+
+        server = new UndertowJaxrsServer();
+        estado = Estado.CREADO;
+        prepararDespliegue();
+    }
+
+    public static PioServer instancia() {
+
+        return instancia;
+    }
 
     public static void main(final String[] args) {
 
-        UndertowJaxrsServer server = new UndertowJaxrsServer();
-
-        ResteasyDeployment deployment = new ResteasyDeployment();
-        deployment.setApplication(App.instancia());
-        deployment.setInjectorFactoryClass("org.jboss.resteasy.cdi.CdiInjectorFactory");
-
-        DeploymentInfo deploymentInfo = server.undertowDeployment(deployment, "/");
-        deploymentInfo.setClassLoader(PioServer.class.getClassLoader());
-        deploymentInfo.setDeploymentName("Undertow + Resteasy example");
-        deploymentInfo.setContextPath("/api");
-        deploymentInfo.addListener(Servlets.listener(org.jboss.weld.environment.servlet.Listener.class));
-
-        server.deploy(deploymentInfo);
-
-        Undertow.Builder builder = Undertow.builder()
-            .addHttpListener(8080, "localhost");
-
-        server.start(builder);
-        
-        Fixture.initData();
-        
+        PioServer.instancia().iniciarServer();
         log.info("PioServer iniciado. Presione Ctrl+C para salir.");
+    }
+
+    private void prepararDespliegue() {
+
+        if (estado.equals(Estado.CREADO)) {
+
+            ResteasyDeployment deployment = new ResteasyDeployment();
+            deployment.setApplication(App.instancia());
+            deployment.setInjectorFactoryClass("org.jboss.resteasy.cdi.CdiInjectorFactory");
+
+            DeploymentInfo deploymentInfo = server.undertowDeployment(deployment, "/");
+            deploymentInfo.setClassLoader(PioServer.class.getClassLoader());
+            deploymentInfo.setDeploymentName("Undertow + Resteasy example");
+            deploymentInfo.setContextPath("/api");
+            deploymentInfo.addListener(Servlets.listener(org.jboss.weld.environment.servlet.Listener.class));
+
+            server.deploy(deploymentInfo);
+
+            estado = Estado.DESPLEGADO;
+        }
+    }
+
+    public void iniciarServer() {
+
+        if (estado.equals(Estado.DESPLEGADO)) {
+
+            Undertow.Builder builder = Undertow.builder().addHttpListener(8080, "localhost");
+            server.start(builder);
+
+            Fixture.initData();
+
+            estado = Estado.INICIADO;
+
+        } else {
+            
+            log.warn("PioServer ya estaba iniciado. Usando la instancia existente...");
+        }
+    }
+
+    public void detenerServer() {
+
+        if (estado.equals(Estado.INICIADO)) {
+
+            server.stop();
+            estado = Estado.DESPLEGADO;
+        }
+    }
+
+    private enum Estado {
+
+        CREADO, DESPLEGADO, INICIADO;
     }
 }
--- src/test/java/ar/com/grupoesfera/piopio/uat/PiosUserAcceptanceTest.java
+++ src/test/java/ar/com/grupoesfera/piopio/uat/PiosUserAcceptanceTest.java
@@ -1,38 +1,15 @@
-package ar.com.grupoesfera.piopio;
+package ar.com.grupoesfera.piopio.uat;
 
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
 import java.util.Date;
 
-import org.apache.commons.logging.LogFactory;
 import org.apache.http.HttpStatus;
 import org.hamcrest.Matchers;
 import org.junit.Assert;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
 import com.jayway.jsonpath.matchers.JsonPathMatchers;
 
-import ar.com.grupoesfera.main.PioServer;
-
-public class UserAcceptanceTest {
-
-    @BeforeClass
-    public static void preparar() {
-
-        PioServer.main(null);
-    }
-
-    @Test
-    public void deberiaDecirHolaMundo() throws Exception {
-
-        RespuestaServicio respuesta = invocarServicio("hola");
-        Assert.assertThat(respuesta.getCodigo(), Matchers.is(HttpStatus.SC_OK));
-        Assert.assertThat(respuesta.getTexto(), Matchers.is("Hola, mundo!"));
-    }
+public class PiosUserAcceptanceTest extends UserAcceptanceTest {
 
     @Test
     public void deberiaDarNotImplementedAlLlamarAPiosSinParametros() throws Exception {
@@ -98,91 +75,5 @@ public class UserAcceptanceTest {
         RespuestaServicio respuesta = invocarServicio("pios", "usuario=1");
         Assert.assertThat(respuesta.getCodigo(), Matchers.is(HttpStatus.SC_BAD_REQUEST));
         Assert.assertThat(respuesta.getTexto(), Matchers.is("Pio no creado. El pio no tiene mensaje."));
-    }
-
-    private RespuestaServicio invocarServicio(String urlServicio, String... params) throws Exception {
-
-        final String urlBase = "http://localhost:8080/api/";
-
-        RespuestaServicio respuesta = new RespuestaServicio();
-
-        try {
-
-            URL url = null;
-            HttpURLConnection connection = null;
-
-            if (params.length > 0) {
-
-                String query = "";
-
-                for (String param : params) {
-
-                    query = query + "&" + param;
-                }
-
-                query = query.substring(1);
-
-                url = new URL(urlBase + urlServicio + "?" + query);
-                connection = (HttpURLConnection) url.openConnection();
-                connection.setRequestMethod("POST");
-
-            } else {
-
-                url = new URL(urlBase + urlServicio);
-                connection = (HttpURLConnection) url.openConnection();
-                connection.setRequestMethod("GET");
-            }
-
-            respuesta.setCodigo(connection.getResponseCode());
-
-            if (respuesta.getCodigo() >= HttpStatus.SC_OK && respuesta.getCodigo() < HttpStatus.SC_MOVED_PERMANENTLY) {
-
-                BufferedReader input = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-                respuesta.setTexto(input.readLine());
-
-            } else {
-
-                InputStream errorStream = connection.getErrorStream();
-
-                if (errorStream != null) {
-
-                    BufferedReader input = new BufferedReader(new InputStreamReader(connection.getErrorStream()));
-                    respuesta.setTexto(input.readLine());
-                }
-            }
-
-        } catch (Exception e) {
-
-            LogFactory.getLog(UserAcceptanceTest.class).error(e);
-            Assert.fail("La prueba falló por un error de conexión");
-        }
-
-        return respuesta;
-    }
-
-    public class RespuestaServicio {
-
-        private Integer codigo;
-        private String texto;
-
-        public Integer getCodigo() {
-
-            return codigo;
-        }
-
-        public void setCodigo(Integer codigo) {
-
-            this.codigo = codigo;
-        }
-
-        public String getTexto() {
-
-            return texto;
-        }
-
-        public void setTexto(String texto) {
-
-            this.texto = texto;
-        }
     }
 }
