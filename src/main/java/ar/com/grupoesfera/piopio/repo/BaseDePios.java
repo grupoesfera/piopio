716ae23ccd004b58ec58880bb0b50bf29c625c65
Marcelo Gore <marcelogore@gmail.com>
2019-02-25 14:55:06 -0300

    Usa una transaccion para persistir Pios

--- src/main/java/ar/com/grupoesfera/piopio/repo/BaseDePios.java
+++ src/main/java/ar/com/grupoesfera/piopio/repo/BaseDePios.java
@@ -4,6 +4,7 @@ import java.util.Date;
 import java.util.List;
 
 import ar.com.grupoesfera.main.App;
+import ar.com.grupoesfera.main.Transaction;
 import ar.com.grupoesfera.piopio.modelo.Pio;
 import ar.com.grupoesfera.piopio.modelo.Usuario;
 
@@ -11,7 +12,7 @@ public class BaseDePios {
 
     @SuppressWarnings("unchecked")
     public List<Pio> obtenerTodos() {
-        
+
         return App.instancia().obtenerEntityManager().createQuery("select p from Pio p").getResultList();
     }
 
@@ -19,41 +20,47 @@ public class BaseDePios {
 
         return App.instancia().obtenerEntityManager().find(Pio.class, id);
     }
-    
+
     public synchronized Pio guardarCon(Usuario autor, String mensaje) {
-        
-        Pio nuevoPio = null;
-        
+
+        final Pio nuevoPio;
+
         if (autor != null && mensaje != null) {
-            
+
             nuevoPio = Pio.nuevo().conId(proximoId()).conAutor(autor).conMensaje(mensaje).conFechaCreacion(new Date());
-            App.instancia().obtenerEntityManager().persist(nuevoPio);
-        }
-        
-        return nuevoPio;
-    }
-    
-    private Long proximoId() {
-        
-        Long maxId = null;
-        
-        maxId = (Long) App.instancia().obtenerEntityManager().createQuery("select max(p.id) from Pio p").getSingleResult();
-        
-        if (maxId != null) {
-            
-            maxId += 1L;
-            
+            Transaction.run((entities) -> {
+                entities.persist(nuevoPio);
+                return null;
+            });
         } else {
             
+            nuevoPio = null;
+        }
+
+        return nuevoPio;
+    }
+
+    private Long proximoId() {
+
+        Long maxId = null;
+
+        maxId = (Long) App.instancia().obtenerEntityManager().createQuery("select max(p.id) from Pio p").getSingleResult();
+
+        if (maxId != null) {
+
+            maxId += 1L;
+
+        } else {
+
             maxId = 1L;
         }
-        
+
         return maxId;
     }
-    
+
     @SuppressWarnings("unchecked")
     public List<Pio> obtenerPor(Usuario autor) {
-        
+
         return App.instancia().obtenerEntityManager().createQuery("select p from Pio p where autor = :autor").setParameter("autor", autor).getResultList();
     }
 }
