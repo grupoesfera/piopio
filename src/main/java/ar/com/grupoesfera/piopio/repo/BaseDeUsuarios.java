f96a03b54707aa62484ae5cad163ee7ce152b33a
algiorgi <eze.giorgi@gmail.com>
2019-07-03 16:46:54 -0300

    Test para la base de usuarios

--- src/main/java/ar/com/grupoesfera/piopio/repo/BaseDeUsuarios.java
+++ src/main/java/ar/com/grupoesfera/piopio/repo/BaseDeUsuarios.java
@@ -1,5 +1,38 @@
 package ar.com.grupoesfera.piopio.repo;
 
+import java.util.List;
+
+import ar.com.grupoesfera.main.App;
+import ar.com.grupoesfera.piopio.modelo.Usuario;
+
 public class BaseDeUsuarios {
+
+    @SuppressWarnings("unchecked")
+    public List<Usuario> obtenerTodos() {
+        return App.instancia().obtenerSesion().createQuery("from Usuario u").getResultList();
+    }
+
+    @SuppressWarnings("unchecked")
+    public List<Usuario> obtenerSeguidoresDe(Usuario usuario) {
+        return App.instancia().obtenerSesion().createQuery("from Usuario u where :usuario member of u.seguidos")
+                                              .setParameter("usuario", usuario)
+                                              .getResultList();
+    }
+
+    @SuppressWarnings("unchecked")
+    public List<Usuario> obtenerAislados() {
+        return App.instancia().obtenerSesion().createQuery("from Usuario u where u.seguidos is empty and not exists (select u2 from Usuario u2 where u member of u2.seguidos)")
+                .getResultList();
+    }
+
+    public Usuario obtenerPor(Long id) {
+        return App.instancia().obtenerSesion().get(Usuario.class, id);
+    }
+    
+    @SuppressWarnings("unchecked")
+    public List<String> obtenerNombres() {
+        
+        return App.instancia().obtenerSesion().createNativeQuery("call nombres()").getResultList();
+    } 
 
 }
--- src/test/java/ar/com/grupoesfera/piopio/BaseDeUsuariosTest.java
+++ src/test/java/ar/com/grupoesfera/piopio/BaseDeUsuariosTest.java
@@ -1,13 +1,20 @@
 package ar.com.grupoesfera.piopio;
 
+import java.util.List;
+
+import org.hamcrest.Matchers;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
 import ar.com.grupoesfera.main.Fixture;
+import ar.com.grupoesfera.piopio.modelo.Usuario;
+import ar.com.grupoesfera.piopio.repo.BaseDeUsuarios;
 
 public class BaseDeUsuariosTest {
+    
+    private BaseDeUsuarios usuarios = new BaseDeUsuarios();
     
     @Before
     public void agregarDatos() {
@@ -20,10 +27,49 @@ public class BaseDeUsuariosTest {
         
         Fixture.dropData();
     }
-
+    
     @Test
-    public void deberiasEscribirPruebasUnitarias() {
+    public void deberiaObtenerTodosLosUsuarios() {
+
+        List<Usuario> todosLosUsuarios = usuarios.obtenerTodos();
+        Assert.assertThat(todosLosUsuarios, Matchers.hasSize(7));
+    }
+    
+    @Test
+    public void deberiaObtenerLosSeguidoresDeUnUusario() {
+
+        Usuario marcelo = Usuario.nuevo().conId(1L).conNombre("Marcelo");
+        List<Usuario> seguidoresDeMarcelo = usuarios.obtenerSeguidoresDe(marcelo);
+        Assert.assertThat(seguidoresDeMarcelo, Matchers.hasSize(3));
+    }
+    
+    @Test
+    public void deberiaEncontrarUsuariosAislados() {
         
-        Assert.fail("Tus clases deberían estar probadas y con Cobertura completa");
+        List<Usuario> aislados = usuarios.obtenerAislados();
+        Assert.assertThat(aislados, Matchers.hasSize(1));
+    }
+    
+    @Test
+    public void deberiaObtenerUnUsuarioExistente() {
+        
+        Long id = 1L;
+        Usuario usuario = usuarios.obtenerPor(id);
+        Assert.assertThat(usuario.getNombre(), Matchers.is("Marcelo"));
+    }
+    
+    @Test
+    public void deberiaDarNullSiElUsuarioNoExiste() {
+        
+        Long id = 30L;
+        Usuario usuario = usuarios.obtenerPor(id);
+        Assert.assertThat(usuario, Matchers.nullValue());
+    }
+    
+    @Test
+    public void deberiaObtenerTodosLosNombres() {
+        
+        List<String> nombres = usuarios.obtenerNombres();
+        Assert.assertThat(nombres, Matchers.containsInAnyOrder("Marcelo", "Brenda", "India", "Leon", "Sebastian", "Santiago", "Alejandro"));
     }
 }
