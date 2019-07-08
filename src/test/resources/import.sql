5ce67f5d56c5a9f08bd975fdef3d49a15469aba4
algiorgi <alejandro.giorgi@grupoesfera.com.ar>
2019-07-08 18:54:18 -0300

    Agrega sp con un parametro

--- src/test/resources/import.sql
+++ src/test/resources/import.sql
@@ -41,3 +41,8 @@
 
 DROP ALIAS IF EXISTS NOMBRES;
 CREATE ALIAS NOMBRES AS $$ java.sql.ResultSet nombres(java.sql.Connection connection) throws java.sql.SQLException { return connection.createStatement().executeQuery("select nombre from usuario"); } $$;
+
+DROP ALIAS IF EXISTS CONTAR_NUMERO_DE_FAVORITOS_DE_UN_PIO;
+CREATE ALIAS CONTAR_NUMERO_DE_FAVORITOS_DE_UN_PIO AS $$ java.sql.ResultSet numeroDeFavortiosDeUnPio(java.sql.Connection connection, java.lang.Long idPio) throws java.sql.SQLException { return connection.createStatement().executeQuery("select count(*) from favorito f where f.pioId = " + idPio + " group by f.pioId"); } $$
+
+ 
