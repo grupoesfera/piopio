df4db989d71c85ad07e4e740aeca09da5fd9a18e
algiorgi <alejandro.giorgi@grupoesfera.com.ar>
2019-07-09 12:25:58 -0300

    Elimina metodos de la base de Comentarios

--- src/main/java/ar/com/grupoesfera/piopio/repo/BaseDeComentarios.java
+++ src/main/java/ar/com/grupoesfera/piopio/repo/BaseDeComentarios.java
@@ -1,51 +1,5 @@
 package ar.com.grupoesfera.piopio.repo;
 
-import java.util.List;
-
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Root;
-
-import org.hibernate.Session;
-
-import ar.com.grupoesfera.main.App;
-import ar.com.grupoesfera.piopio.modelo.Comentario;
-import ar.com.grupoesfera.piopio.modelo.Comentario_;
-import ar.com.grupoesfera.piopio.modelo.Usuario;
-
 public class BaseDeComentarios {
 
-    public List<Comentario> obtenerTodos() {
-        
-        Session session = App.instancia().obtenerSesion();
-        
-        CriteriaBuilder criteriaBuilder = session.getCriteriaBuilder();
-        CriteriaQuery<Comentario> query = criteriaBuilder.createQuery(Comentario.class);
-        Root<Comentario> comentarios = query.from(Comentario.class);
-        query.select(comentarios);
-        
-        return session.createQuery(query).getResultList();
-    }
-
-    public List<Comentario> obtenerRealizadosPor(Usuario autor) {
-        
-        Session session = App.instancia().obtenerSesion();
-        CriteriaBuilder criteriaBuilder = session.getCriteriaBuilder();
-        CriteriaQuery<Comentario> query = criteriaBuilder.createQuery(Comentario.class);
-        
-        Root<Comentario> comentarios = query.from(Comentario.class);
-        
-        query.select(comentarios)
-            .where(criteriaBuilder.equal(comentarios.get(Comentario_.autor), autor));
-        
-               return session.createQuery(query).getResultList();
-    }
-
-    public long contarComentariosRealizadosPor(Usuario usuario) {
-        
-        return App.instancia().obtenerSesion()
-                    .createNamedQuery("contarComentariosRealizadosPor", Long.class)
-                    .setParameter("autor", usuario)
-                    .getSingleResult();
-    }
 }
